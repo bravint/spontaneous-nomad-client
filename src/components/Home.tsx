9636f38864f913526ab71e9748c0e91f2a77c043
bravint <bravint@gmail.com>
2022-04-01 19:39:07 +0100

    edit hero, add login call to action

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,16 +4,27 @@ import '../styles/home.css';
 
 export const Home = () => {
     return (
-        <section className="home">
-            <div className="home-hero">
+        <>
+            <section className="home">
                 <Header />
-                <h1 className="home-hero-text">
-                    Share
-                    <br />
-                    your favourite <br />
-                    destinations
-                </h1>
-            </div>
-        </section>
+                <div className="home-hero">
+                    <div className="home-hero-text-container">
+                        <h1 className="home-hero-text">
+                            Share
+                            <br />
+                            your favourite <br />
+                            destinations
+                        </h1>
+                    </div>
+                </div>
+                <div className="home-call-action">
+                    <p>Track and Share your experiences today!</p>
+                    <div className="home-call-action-auth">
+                        <p className="home-auth-button">Login</p>
+                        <p className="home-auth-button">Register</p>
+                    </div>
+                </div>
+            </section>
+        </>
     );
 };
