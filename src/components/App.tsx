53cef7523c9df520395d149eec5ec97f62e5e3af
bravint <bravint@gmail.com>
2022-04-01 17:49:31 +0100

    react-router-dom, add routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,22 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
 import { Home } from './Home';
+import { Login } from './Login';
+import { Register } from './Register';
+import { Footer } from './Footer';
 
 import '../styles/app.css';
 
 export const App = () => {
     return (
         <div className="app">
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/login" element={<Login />}></Route>
+                <Route path="/register" element={<Register />}></Route>
+            </Routes>
+            <Footer />
         </div>
     );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,17 +3,18 @@ import React, { useState } from 'react';
 import '../styles/header.css';
 
 export const Header = () => {
+
     return (
         <header>
             <div className="header-title">Spontanous Nomad</div>
-            <ul className="header-site-links">
+            <ul className="header-site-link">
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
-            <ul className="header-auth-links">
-                <li>Login</li>
-                <li>Register</li>
+            <ul className="header-auth-link">
+                <li className="header-auth-button">Login</li>
+                <li className="header-auth-button">Register</li>
             </ul>
         </header>
     );
