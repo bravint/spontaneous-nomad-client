b616e0f48aaaf94c28ca01888996165c4220ad41
bravint <bravint@gmail.com>
2022-04-02 10:43:55 +0100

    add css root variables

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App';
 
 import './styles/reset.css';
+import './styles/root.css'
 
 ReactDOM.render(
     <BrowserRouter>
