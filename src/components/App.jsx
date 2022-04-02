dfbf5fcfe2cf063cbf0d07e3083eeeebdd7d835f
bravint <bravint@gmail.com>
2022-04-02 15:40:38 +0100

    remove deps array from useEffect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -68,7 +68,7 @@ export const App = () => {
         };
 
         fetchUserFromToken();
-    }, []);
+    });
 
     return (
         <StoreContext.Provider value={{ state: state, dispatch: dispatch }}>
--- src/components/oauth/OauthSuccess.tsx
+++ src/components/oauth/OauthSuccess.tsx
@@ -43,7 +43,7 @@ export const OAuthSuccess = () => {
         };
 
         fetchUserFromToken();
-    }, []);
+    });
 
     return (
         <div>
