b86ece0532b348fac74077542ae934632256123a
bravint <bravint@gmail.com>
2022-04-17 15:23:27 +0100

    remove facebook login placeholder

--- src/components/oauth/OAuthLogin.tsx
+++ src/components/oauth/OAuthLogin.tsx
@@ -1,4 +1,3 @@
-import facebook from '../../assets/auth/facebook-sml.svg';
 import google from '../../assets/auth/google-sml.svg';
 
 import '../../styles/oauth-login.css';
@@ -9,8 +8,6 @@ export const OAuthLogin = () => {
     const handleGoogleLogin = () =>
         window.open(SERVER_URL.OAUTH_GOOGLE, '_self');
 
-    const handleFacebookLogin = () => window.open('#', '_self');
-
     return (
         <div className="oauth-container">
             <div
@@ -19,17 +16,6 @@ export const OAuthLogin = () => {
             >
                 <img className="oauth-logo" src={google} alt="Google Logo" />
                 <p className="oauth-text">Sign in With Google</p>
-            </div>
-            <div
-                className="oauth-provider oauth-provider-facebook"
-                onClick={handleFacebookLogin}
-            >
-                <img
-                    className="external-auth-logo"
-                    src={facebook}
-                    alt="Facebook Logo"
-                />
-                <p className="oauth-text">Sign in With Facebook</p>
             </div>
         </div>
     );
