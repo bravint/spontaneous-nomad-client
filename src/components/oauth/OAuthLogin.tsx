bac542582c820b4b9c76ea5b275e361975546de2
bravint <bravint@gmail.com>
2022-04-17 15:28:35 +0100

    refactor google oauth login

--- src/components/oauth/OAuthLogin.tsx
+++ src/components/oauth/OAuthLogin.tsx
@@ -5,18 +5,15 @@ import '../../styles/oauth-login.css';
 import { SERVER_URL } from '../../utils/config';
 
 export const OAuthLogin = () => {
-    const handleGoogleLogin = () =>
-        window.open(SERVER_URL.OAUTH_GOOGLE, '_self');
+    const handleGoogleLogin = () => window.open(SERVER_URL.OAUTH_GOOGLE, '_self');
 
     return (
-        <div className="oauth-container">
-            <div
-                className="oauth-provider oauth-provider-google"
-                onClick={handleGoogleLogin}
-            >
-                <img className="oauth-logo" src={google} alt="Google Logo" />
-                <p className="oauth-text">Sign in With Google</p>
-            </div>
+        <div
+            className="oauth-provider oauth-provider-google"
+            onClick={handleGoogleLogin}
+        >
+            <img className="oauth-logo" src={google} alt="Google Logo" />
+            <p className="oauth-text">Sign in With Google</p>
         </div>
     );
 };
