877e3ccb48aedcda71f38eef545ae58bc3e30984
bravint <bravint@gmail.com>
2022-04-28 09:15:41 +0100

    edit dashboard

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -11,63 +11,63 @@ import { StoreContext, initialState } from '../utils/store';
 import '../styles/dashboard.css';
 
 export const Dashboard = () => {
-    const { dispatch } = useContext(StoreContext);
+    // const { dispatch } = useContext(StoreContext);
 
-    const handleDispatch = (type: string, payload: IFriend | null | number | string): void => {
-        dispatch({
-            type: type,
-            payload: payload,
-        });
-    };
+    // const handleDispatch = (type: string, payload: IFriend | null | number | string): void => {
+    //     dispatch({
+    //         type: type,
+    //         payload: payload,
+    //     });
+    // };
 
-    useEffect(() => {
-        const jwt = localStorage.getItem(LOCAL_STORAGE.JWT);
+    // useEffect(() => {
+    //     const jwt = localStorage.getItem(LOCAL_STORAGE.JWT);
 
-        const fetchLocations = async () => {
-            const response = await fetch(SERVER_URL.LOCATION, {
-                method: HTTP_METHOD.GET,
-                headers: {
-                    Authorization: HTTP_AUTH_TYPE.BEARER + jwt,
-                },
-            });
+    //     const fetchLocations = async () => {
+    //         const response = await fetch(SERVER_URL.LOCATION, {
+    //             method: HTTP_METHOD.GET,
+    //             headers: {
+    //                 Authorization: HTTP_AUTH_TYPE.BEARER + jwt,
+    //             },
+    //         });
 
-            const result = await response.json();
+    //         const result = await response.json();
 
-            if (result.data) {
-                handleDispatch(STORE_ACTIONS.LOCATIONS, result.data);
-            }
-        };
+    //         if (result.data) {
+    //             handleDispatch(STORE_ACTIONS.LOCATIONS, result.data);
+    //         }
+    //     };
 
-        const fetchFriends = async () => {
-            const response = await fetch(SERVER_URL.FOLLOW, {
-                method: HTTP_METHOD.GET,
-                headers: {
-                    Authorization: HTTP_AUTH_TYPE.BEARER + jwt,
-                },
-            });
+    //     const fetchFriends = async () => {
+    //         const response = await fetch(SERVER_URL.FOLLOW, {
+    //             method: HTTP_METHOD.GET,
+    //             headers: {
+    //                 Authorization: HTTP_AUTH_TYPE.BEARER + jwt,
+    //             },
+    //         });
 
-            const result = await response.json();
+    //         const result = await response.json();
 
 
-            if (result.data) {
-                handleDispatch(STORE_ACTIONS.FRIENDS, result.data);
-            }
-        };
+    //         if (result.data) {
+    //             handleDispatch(STORE_ACTIONS.FRIENDS, result.data);
+    //         }
+    //     };
 
-        fetchLocations();
-        fetchFriends();
-        handleDispatch(STORE_ACTIONS.FRIEND_ID, initialState.friendId);
-        handleDispatch(STORE_ACTIONS.FRIEND_NAME, initialState.friendName);
-        handleDispatch( STORE_ACTIONS.SELECTED_LOCATION, initialState.selectedLocation);
-    }, []);
+    //     fetchLocations();
+    //     fetchFriends();
+    //     handleDispatch(STORE_ACTIONS.FRIEND_ID, initialState.friendId);
+    //     handleDispatch(STORE_ACTIONS.FRIEND_NAME, initialState.friendName);
+    //     handleDispatch( STORE_ACTIONS.SELECTED_LOCATION, initialState.selectedLocation);
+    // }, []);
 
     return (
         <section className="dashboard">
-            <section className="dashboard-main">
+            {/* <section className="dashboard-main">
                 <Map />
                 <FriendsList />
             </section>
-            <Sidebar />
+            <Sidebar /> */}
         </section>
     );
 };
